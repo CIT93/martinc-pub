57fc65acf7d8352043ce7256415573371810bd0a
Martin <Martin.Estevan@Gmail.com>
2024-08-23 00:00:03 -0700

    coding is done

--- w2/main.js
+++ w2/main.js
@@ -1,2 +1,22 @@
-const myHeading = document.querySelector("h1")
-myHeading.textContent = ""
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "";
+
+// 1. members of household
+const myHouseHold = 12;
+// 2. size of home
+const myHomeSize = 7;
+// 3. food choices
+const myFoodIntake = 8;
+// 4. water consumption
+const myWaterIntake = 1;
+// 5. household purchases each year
+const myPurchases = 4; 
+// 6. waste produced
+const myWaste = 5;
+// 7. waste recycled
+const myRecycle = 8;
+// 8. annual transportation scores
+const myTransport = 6;
+// 9. add points
+const cfpTotal = myHouseHold + myHomeSize + myFoodIntake + myWaterIntake + myPurchases + myWaste + myRecycle + myTransport
+// 10. 
