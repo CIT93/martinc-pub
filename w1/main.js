bf3d0cf8cc96f86177c02af15a7bc6eb086ee4ba
Martin <Martin.Estevan@Gmail.com>
2024-08-26 13:31:55 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log ('Hello World')
+console.log ("Hello from inside the main.js file")
+
+// let myVar;
+
+const myVar = "Martin (string)";
+const myVarType = typeof(myVar);
+console.log("myVarType" + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+if(myVarType === "number") {
+    console.log(`will line 11 run`);
+} else {
+    console.log(`will line 13 run`);
+}
+
+function runNow () {
+    console.log(`inside the function`);
+}
+
+runNow();
+runNow();
