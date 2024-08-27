66b40cf0ef00f07e4f35a3bc1f8ecfeefcc6eb41
Martin <Martin.Estevan@Gmail.com>
2024-08-26 23:03:58 -0700

    quotes, operators, and primitives

---
+++ w3/main.js
@@ -0,0 +1,23 @@
+const myName = "Martin Cisneros";
+const myAge = 34.0177;
+
+const jsIsWeird = myName + myAge;
+
+console.log(jsIsWeird);
+console.log(typeof jsIsWeird);
+console.log(typeof myAge);
+
+// what is order of presedence
+// write an example
+
+// const results = 
+// console.log(results)
+
+const amIhungry = true;
+console.log(typeof amIhungry);
+
+let myVar;
+console.log(typeof myVar);
+
+const myHeading = document.querySelector("h1");
+console.log(typeof myHeading);
