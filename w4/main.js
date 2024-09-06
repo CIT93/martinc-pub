3de492c8d1e940510d36508bd9d711af04fe377e
Martin <Martin.Estevan@Gmail.com>
2024-09-05 23:58:18 -0700

    updated output to DOM

--- w4/main.js
+++ w4/main.js
@@ -11,7 +11,7 @@ function determineHouseSizePts(size) {
     } else if(size === "apt") {
         houseSizePoints = 2;
     }
-    return houseSizePoints;
+    return houseSizePoints
   }
 
   function determineHouseholdPts(numberInhousehold) {
@@ -31,23 +31,37 @@ function determineHouseSizePts(size) {
       } else if(numberInhousehold > 6) {
         houseHoldPoints = 2;
       }
-      return houseHoldPoints;
-    }
+    return houseHoldPoints
+  }
 
-    function start(householdMembers, houseSize) {
-      const houseHoldPoints = determineHouseholdPts(householdMembers);
-      const houseSizePoints = determineHouseSizePts(houseSize);
-      const total = houseHoldPoints + houseSizePoints;
-      cfpData.push([householdMembers, houseSize, houseHoldPoints, houseSizePoints, total]);
-    }
+  function start(householdMembers, houseSize) {
+    const houseHoldPoints = determineHouseholdPts(householdMembers);
+    const houseSizePoints = determineHouseSizePts(houseSize);
+    const total = houseHoldPoints + houseSizePoints;
+    cfpData.push([householdMembers, houseSize, houseHoldPoints, houseSizePoints, total]);
+  }
 
-    function displayOutput() {
-    
+  function displayOutput() {
+    for (arr of cfpData){
+      console.log(arr)
+      const output = document.getElementById("output");
+      const newP = document.createElement("p");
+      newP.textContent = `Carbon footprint total is ${arr[4]}`;
+      output.appendChild(newP);
+      output.appendChild(newP1);
+      output.appendChild(newP2);
+      output.appendChild(newP3);
+      output.appendChild(newP4);
+      output.appendChild(newP5);
+    }
   }
 
     start(5, "apt");
     start(4, "large");
     start(3, "medium");
     start(2, "small");
+    start(1);
+    start(6);
+    start(7);
 
     displayOutput()
