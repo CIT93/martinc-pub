54b91954e3070c47d5de25b6db7bdd3f3966bbe3
Martin <Martin.Estevan@Gmail.com>
2024-09-13 08:07:24 -0700

    setup and array of objects

--- w5/main.js
+++ w5/main.js
@@ -34,34 +34,71 @@ function determineHouseSizePts(size) {
     return houseHoldPoints
   }
 
+  function displayOutObj(obj) {
+    console.log(obj);
+    const output = document.getElementById("output");
+    const newH2 = document.createElement("h2");
+    newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
+    output.appendChild(newH2);
+    const newH3 = document.createElement("h3");
+    newH3.textContent = `based on number in and size of home`;
+    const newP = document.createElement("p");
+    newP.textContent = `this number is based on the number of people in the house of ${obj.houseHM} (score: ${obj.HouseHPts}),`;
+    newP.textContent += ` and a ${obj.houseS} size of home (score:${obj.houseSPts}).`;
+    output.appendChild(newH2);
+    output.appendChild(newH3);
+    output.appendChild(newP);
+  }
+
   function start(householdMembers, houseSize) {
     const houseHoldPoints = determineHouseholdPts(householdMembers);
     const houseSizePoints = determineHouseSizePts(houseSize);
     const total = houseHoldPoints + houseSizePoints;
-    cfpData.push([householdMembers,
-      houseSize,
-      houseHoldPoints,
-      houseSizePoints,
-      total,
-    ]);
-  }
+    cfpData.push({
+        houseHM: householdMembers,
+        houseS: houseSize, 
+        HouseHPts: houseHoldPoints, 
+        houseSPts: houseSizePoints, 
+        cfpTotal: total
+    });
+
+   }
+
 
   function displayOutput() {
-    for (let i = 0; i < cfpData.length; i++) {
-      console.log(i)
+    for (obj of cfpData) {
+      console.log(obj)
       const output = document.getElementById("output");
       const newH2 = document.createElement("h2");
-      newH2.textContent = `Carbon Footprint ${cfpData[i][4]}`;
-      // const newH3 = document.createElement("h3");
-      // newH3.textContent = `based on number in and size of home`;
-      // const newP = document.createElement("p");
-      // newP.textContent = `this number is based on the number of people in the house of ${arr[0]} (score: ${arr[3]}),`;
-      // newP.textContent += ` and a ${arr[1]} size of home (score:${arr[2]}).`;
+      newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
+  //     const newH3 = document.createElement("h3");
+  //     newH3.textContent = `based on number in and size of home`;
+  //     const newP = document.createElement("p");
+  //     newP.textContent = `this number is based on the number of people in the house of ${arr[0]} (score: ${arr[3]}),`;
+  //     newP.textContent += ` and a ${arr[1]} size of home (score:${arr[2]}).`;
       output.appendChild(newH2);
-      // output.appendChild(newH3);
-      // output.appendChild(newP);
+  //     output.appendChild(newH3);
+  //     output.appendChild(newP);
     }
   }
+
+
+  // function displayOutput() {
+  //   for (let i = 0; i < cfpData.length; i++) {
+  //     console.log(i)
+  //     const output = document.getElementById("output");
+  //     const newH2 = document.createElement("h2");
+  //     newH2.textContent = `Carbon Footprint ${cfpData[i][4]}`;
+  //     const newH3 = document.createElement("h3");
+  //     newH3.textContent = `based on number in and size of home`;
+  //     const newP = document.createElement("p");
+  //     newP.textContent = `this number is based on the number of people in the house of ${arr[0]} (score: ${arr[3]}),`;
+  //     newP.textContent += ` and a ${arr[1]} size of home (score:${arr[2]}).`;
+  //     output.appendChild(newH2);
+  //     output.appendChild(newH3);
+  //     output.appendChild(newP);
+  //   }
+  // }
 
     start(5, "apt");
     start(4, "large");
@@ -69,44 +106,3 @@ function determineHouseSizePts(size) {
     start(2, "small");
 
     displayOutput()
-
-
-    // object coding challenge - refactor start function to store the data in an object isntead of an array
-
-
-// Intro to object
-
-const myArr = [];
-const myObj = {
-  cfpTotal: 18,
-  houseSize: "small",
-  displayOut: function(){
-    console.log("this is a method call");
-    console.log(this.houseSize);
-    console.log(myObj.cfpTotal);
-  }
-};
-// console.log(myObj.cfpTotal);
-// console.log(myObj["houseSize"]);
-myObj.displayOut()
-
-
-   // for (initialization; condition; afterthought)
-    //  statement
-
-// how would you modify this code to count from 1 to 15?
-    //for (let i = 0; i < 16; i++) {
-      //console.log(i);
-    //}
-    // I did it! I changed it to i < 16 and it began to count to 15. a lucky and educated guess
-
-
-    for (let i = 6; i >= 0; i--) {
-            console.log(i);
-          }
-
-
-    // standard for loop
-
-
-    // refactor for of loop above into standard for loop
