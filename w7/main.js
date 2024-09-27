87ca585fdbffda618771ea93f3a3933ba762f4cc
Martin <Martin.Estevan@Gmail.com>
2024-09-26 23:56:38 -0700

    setup done

---
+++ w7/main.js
@@ -0,0 +1,95 @@
+import { renderTbl, renderTblHeading } from "./render";
+
+const FORM = document.getElementById("form")
+const OUTPUT = document.getElementById("output");
+
+const cfpData = [];
+
+function determineHouseSizePts(size) {
+    let houseSizePoints = 0;
+    if(size === "large") {
+        houseSizePoints = 10;
+    } else if(size === "medium") {
+        houseSizePoints = 7;
+    } else if(size === "small") {
+        houseSizePoints = 4;
+    } else if(size === "apt") {
+        houseSizePoints = 2;
+    }
+    return houseSizePoints
+  }
+
+  function determineHouseholdPts(numberInhousehold) {
+      let houseHoldPoints = 0;
+      if (numberInhousehold === 1) {
+        houseHoldPoints = 14
+      } else if(numberInhousehold === 2) {
+        houseHoldPoints = 12;
+      } else if(numberInhousehold === 3) {
+        houseHoldPoints = 10;
+      } else if(numberInhousehold === 4) {
+        houseHoldPoints = 8;
+      } else if(numberInhousehold === 5) {
+        houseHoldPoints = 6;
+      } else if(numberInhousehold === 6) {
+        houseHoldPoints = 4;
+      } else if(numberInhousehold > 6) {
+        houseHoldPoints = 2;
+      }
+    return houseHoldPoints;
+  }
+
+  function start(firstName, lastName, householdMembers, houseSize) {
+    const houseHoldPoints = determineHouseholdPts(householdMembers);
+    const houseSizePoints = determineHouseSizePts(houseSize);
+    const total = houseHoldPoints + houseSizePoints;
+    cfpData.push({
+        houseHM: householdMembers,
+        houseS: houseSize, 
+        houseHPts: houseHoldPoints, 
+        houseSPts: houseSizePoints, 
+        cfpTotal: total,
+        firstName: firstName,
+        lastName: lastName
+    });
+  }
+
+  function displayOutput() {
+    for (obj of cfpData) {
+      const newH1 = document.createElement("h1");
+      newH1.textContent = `${obj.firstName} ${obj.lastName}`;
+      const newH2 = document.createElement("h2");
+      newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
+      const newH3 = document.createElement("h3");
+      newH3.textContent = `based on number in and size of home`;
+      const newP = document.createElement("p");
+      newP.textContent = `this number is based on the number of people in the house of ${obj.houseHM} (score: ${obj.houseHPts}),`;
+      newP.textContent += ` and a ${obj.houseS} size of home (score:${obj.houseSPts}).`;
+      OUTPUT.appendChild(newH1);
+      OUTPUT.appendChild(newH2);
+      OUTPUT.appendChild(newH3);
+      OUTPUT.appendChild(newP);
+    }
+  }
+
+  FORM.addEventListener('submit', function(e){
+      e.preventDefault();
+      const firstName = FORM.firstname.value;
+      const lastName = FORM.lastname.value;
+      const householdMembers = parseInt(FORM.housem.value);
+      const houseSize = FORM.houses.value;
+      start(firstName, lastName, householdMembers, houseSize);
+      OUTPUT.innerHTML = "";
+      //displayOutput();
+      renderTbl(cfpData);
+      FORM.reset();
+    })
+
+    // is the apartment score correct? if not, why not?
+    // no it is not. Because it was not getting the cprrect reference in the index for the aprtment value "apt" in the js
+
+    // why are we doing all this work in the form to make sure the user gives us good data?
+    // assume the user is not giving good data. using html to save us from writing a lot of javascript because its built in . make sure we're getting good data from the user
+
+    // I originally tried to rewrite the displayOutput function to cut down on "expensive" code by trying to refrence the tbl-data but could not get it working. did not think to create a new function with the TblHeading and put the data from the renderTbl data in there then appending what was left
+// I could not figure out why my code was not working until devtools indicated it was because my const trTextArr = ["Martin", 2, "Medium", 19]; was with brackets instead of parenthesis. Also my text blended in with my background color so I had to change that as well after I highlighted my outputs in devtools
---
+++ w7/render.js
@@ -0,0 +1,43 @@
+const TBL = document.getElementById("tab-data")
+
+function renderTblHeading(){
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tr = document.createElement("tr");
+    const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Actions"];
+    headingTextArr.forEach(function(text){
+      const th = document.createElement("th");
+      th.textContent = text;
+      tr.appendChild(th);
+    });
+    thead.appendChild(tr);
+    table.appendChild(thead);
+    TBL.appendChild(table);
+    return table
+  }
+
+  function renderTbl(data){
+    const table = renderTblHeading();
+    const tbody = document.createElement("tbody");
+    const tr = document.createElement("tr");
+    const trTextArr = ["Martin", 2, "Medium", 19];
+    trTextArr.forEach(function(text){
+      const td = document.createElement("td");
+      td.textContent = text;
+      tr.appendChild(td);
+    })
+    const td = document.createElement("td");
+    const btnEdit = document.createElement("button");
+    const btnDelete = document.createElement("button");
+    btnEdit.textContent = "Edit";
+    btnDelete.textContent = "Delete";
+    td.appendChild(btnEdit);
+    td.appendChild(btnDelete);
+    tr.appendChild(td);
+    tbody.appendChild(tr)
+    table.appendChild(tbody);
+    TBL.appendChild(table);
+
+  }
+
+  export {renderTbl, renderTblHeading}
